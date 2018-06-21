1cb0de265a01de477d64bee2e1832e7471b11bcc
Jonathan Zarate <zaratedev@gmail.com>
2018-06-21 13:22:06 -0500

    Getters and Setters in PHP. Created Class Person.

---
+++ Person.php
@@ -0,0 +1,37 @@
+<?php
+
+  /**
+   *  Class Person
+   */
+  class Person
+  {
+    public $name;
+    public $age;
+
+    function __construct($name)
+    {
+      $this->name = $name;
+    }
+
+    public function getAge()
+    {
+      return $this->age;
+    }
+
+    public function setAge($age)
+    {
+      if ($age < 18) {
+        throw new \Exception("Person is not old enough.");
+      }
+      $this->age = $age;
+    }
+  }
+
+  $jonh = new Person('jonathan');
+
+  $jonh->setAge(24);
+
+  var_dump($jonh->getAge());
+
+
+ ?>
